17f32c322e418d1f932f3b30683c2d0a8be1335f
Roman Yarlykov <roman.yarlykov@metalamp.io>
2024-06-05 10:17:12 +0500

    init subgraph

--- src/trustless-otc.ts
+++ src/trustless-otc.ts
@@ -1,67 +1,45 @@
 import {
-  OfferCancelled as OfferCancelledEvent,
-  OfferCreated as OfferCreatedEvent,
-  OfferTaken as OfferTakenEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
-} from "../generated/TrustlessOTC/TrustlessOTC"
-import {
-  OfferCancelled,
-  OfferCreated,
-  OfferTaken,
-  OwnershipTransferred
-} from "../generated/schema"
+    OfferCancelled as OfferCancelledEvent,
+    OfferCreated as OfferCreatedEvent,
+    OfferTaken as OfferTakenEvent,
+} from '../generated/TrustlessOTC/TrustlessOTC';
+import { OfferCancelled, OfferCreated, OfferTaken } from '../generated/schema';
 
 export function handleOfferCancelled(event: OfferCancelledEvent): void {
-  let entity = new OfferCancelled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tradeID = event.params.tradeID
+    let entity = new OfferCancelled(
+        event.transaction.hash.concatI32(event.logIndex.toI32()),
+    );
+    entity.tradeID = event.params.tradeID;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+    entity.save();
 }
 
 export function handleOfferCreated(event: OfferCreatedEvent): void {
-  let entity = new OfferCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tradeID = event.params.tradeID
+    let entity = new OfferCreated(
+        event.transaction.hash.concatI32(event.logIndex.toI32()),
+    );
+    entity.tradeID = event.params.tradeID;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+    entity.save();
 }
 
 export function handleOfferTaken(event: OfferTakenEvent): void {
-  let entity = new OfferTaken(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tradeID = event.params.tradeID
+    let entity = new OfferTaken(
+        event.transaction.hash.concatI32(event.logIndex.toI32()),
+    );
+    entity.tradeID = event.params.tradeID;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
 
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+    entity.save();
 }
